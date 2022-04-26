6989008c293949c8d586651685194e05f2af7790
Vinhlam2512 <lamvinh2431@gmail.com>
2022-04-27 00:02:17 +0700

    Parallax

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
     content: ['./public/**/*.{html,js}'],
     theme: {
+        screens: {
+            xs: '480px',
+            ...defaultTheme.screens,
+        },
         extend: {
-            screens:{
-                'xs': '480px',
-            },
             fontFamily: {
                 Karla: ['Karla', 'sans-serif'],
             },
